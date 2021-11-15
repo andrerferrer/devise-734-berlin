a75db59899684a1a2466c506b11cdbc431c0e478
Andre Ferrer <andrerferrer@hotmail.com>
2021-11-15 11:34:09 +0100

    finish lovecode

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+
+  # BOTH ARE THE SAME
+  # before_action :authenticate_user!, except: %i[ home ]
+  skip_before_action :authenticate_user!, only: :home
+
   def home
   end
 end
